1ec08ac04c78ca5d76fe8aaab84d88f8ebf36deb
vlagou <112933180+vlagou@users.noreply.github.com>
2023-04-05 01:00:22 +0200

    ajout fonction generer_sous_dataset

--- API/preprocessing.py
+++ API/preprocessing.py
@@ -45,3 +45,19 @@ def data_transformation(df):
     
     # Retourne le dataset transformé ainsi que les valeurs minimales et maximales pour renvoyer la bonne prédiction future
     return df_scaled, prix_min, prix_max, n_pieces_min, n_pieces_max, surface_habitable_min, surface_habitable_max
+
+
+def generer_sous_dataset(df,surface,lim_nb_ligne=50):
+
+    df["date_transaction"] = df.date_transaction.apply(lambda x:pd.to_datetime(x))
+    if (surface ==None):
+        df['sort_surf']=None
+    else:
+        df['sort_surf'] = df.surface_habitable.apply(lambda x: abs(x - surface))
+    df['semester']= df.date_transaction.dt.year.astype(str) + 'S'+ np.where(df.date_transaction.dt.quarter.gt(2),2,1).astype(str)
+    df["date_transaction"] =df.date_transaction.apply(lambda x:str(x.date()))
+    df=df.sort_values(by=['semester','sort_surf',], ascending=(False,True)).head(lim_nb_ligne)
+    df = df[['prix', 'surface_habitable', 'n_pieces','date_transaction','semester',
+         'vefa','adresse', 'code_postal', 'NOM_COM', 'NOM_IRIS', 'TYP_IRIS', 'latitude', 'longitude']]
+
+    return df
