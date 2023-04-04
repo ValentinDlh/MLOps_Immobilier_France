080d49204a4b09d2d290e4c4358ebafc3f76530f
vlagou <112933180+vlagou@users.noreply.github.com>
2023-04-05 00:41:35 +0200

    Add files via upload

---
+++ BD/db.py
@@ -0,0 +1,95 @@
+#from pydantic import BaseModel
+import pandas as pd
+#from typing import Optional
+#from datetime import date
+import pymongo
+import json
+from bson.json_util import dumps
+
+
+
+mongoURI="mongodb://localhost:27017"
+client=pymongo.MongoClient(mongoURI)
+db=client['Transactions']
+collection=db['Immo']
+
+
+
+def get_one(c:str):
+    return collection.find_one({"_id": c})
+
+def extract_from_DB_to_df_with_condition(condition:dict,col):
+    #df=pd.DataFrame(list(collection.find()))
+    df = pd.DataFrame.from_dict(json.loads(dumps(collection.find(condition))))
+    df.set_index(['id_transaction'])
+    df["date_transaction"] = df.date_transaction.apply(lambda x: pd.to_datetime(x['$date']).date())
+    df=df[col]
+
+    return df
+
+
+def extract_distinct_value_with_condition(condition:dict,v):
+    #return list de valeur unique sur une requête avec condition
+    return collection.find(condition).distinct(v)
+
+def extract_from_DB_to_df(col):
+    #df = pd.DataFrame(list(collection.find()))
+    df = pd.DataFrame.from_dict(json.loads(dumps(collection.find())))
+    df.set_index(['id_transaction'])
+    df["date_transaction"] = df.date_transaction.apply(lambda x: str(pd.to_datetime(x['$date']).date()))
+    df=df[col]
+    return df
+
+def agg_from_DB_to_df(condition: dict, col):
+    #l=[]
+    #retourne liste dpt, list quartier pour un dpt
+    #df = pd.DataFrame.from_dict(json.loads(dumps(collection.find(condition))))
+    #df.set_index(['id_transaction'])
+    #df = df[col]
+    return None
+
+def generate_tdb_quartier():
+    return None
+
+    #return collection.find(condition)
+
+#ajouter une colonne dans la BD prix/moyen par m²
+#Créer nouvelle table
+#_id
+#quartier
+#year
+#prix moyen/m²
+#prix median/m²
+#ecart_type m²
+#Volume transac
+#
+
+'''class Transaction(BaseModel):
+
+    #ID : StringField(default=str(ObjectIdField()))
+    #adresse:str
+    #code_postal:int
+    #date_transaction:date
+    #DCOMIRIS:str
+    #departement:str
+    #prix:Optional[float]=None
+    #surface_habitable:int
+    #DEPCOM:str
+    #geometry:Point
+    #id_transaction:str
+    #id_ville:int
+    #IRIS:str
+    #latitude:str
+    #longitude:str
+    #n_pieces:Optional[int]=None
+    #NOM_COM:str
+    #NOM_IRIS:Optional[str]=None
+    #TYPE_IRIS:Optional[str]=None
+    #type_batiment:Optional[str]=None
+    #vefa:Optional[bool]
+    #ville:str'''
+
+
+
+
+
