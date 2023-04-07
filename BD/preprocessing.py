2c4873c2bcf22c3a1d62a9dbc035b774ee119c1d
vlagou <112933180+vlagou@users.noreply.github.com>
2023-04-07 17:27:52 +0200

    Update preprocessing.py

--- BD/preprocessing.py
+++ BD/preprocessing.py
@@ -2,25 +2,23 @@ import pandas as pd
 from sklearn.preprocessing import MinMaxScaler
 import db as db
 import numpy as np
-###fichier preprocessing à remplacer par fichier db
+from scipy import stats
+
 
 # Retourne la liste des départements contenu dans transactions
 def get_departements():
-    #departements = list(transactions["departement"].unique())
     dpt=db.extract_distinct_value({},"departement")
     return dpt
 
 
 # Retourne la liste des villes d'un département donné contenu dans transactions
 def get_villes(departement):
-    #villes = list(transactions[transactions["departement"] == departement]["villes"].unique())
     villes = db.extract_distinct_value({'departement':str(departement)},"NOM_COM")
     return villes
 
 
 # Retourne la liste des quartiers d'une ville donnée contenu dans transactions
 def get_quartiers(ville):
-    #quartiers = list(transactions[transactions["villes"] == ville]["NOM_IRIS"].unique())
     quartiers = db.extract_distinct_value({"NOM_COM":ville},"NOM_IRIS")
     return quartiers
 
@@ -28,15 +26,13 @@ def get_quartiers(ville):
 # Renvoie une base de données ciblée en fonction des données de géolocalisation
 
 def data_selection(departement,ville,quartier,col,sous_dataset:bool,param_sous_data:dict):
-    #transactions = transactions[(transactions["departement"] == departement) & (transactions["ville"] == ville) & (
-                #transactions["NOM_IRIS"] == quartier)]
-    # extract_from_DB_to_df_with_condition(condition:dict,col)
+    #On definit query un dictionnaire qui définit les critères géographiques de transactions : ville et quartiers
     query={"departement":departement,"NOM_COM":ville,"NOM_IRIS":{"$in": quartier}}
     transactions=db.extract_from_DB_to_df_with_condition(query,col)
 
+    #fonction generer_sous_dataset va prendre en compte les critères autres que geographique, à savoir : surface, n_pièces, vefa
     if sous_dataset:
         sous_data=generer_sous_dataset(df=transactions,col=col,surface=param_sous_data["surface_habitable"],n_pieces=param_sous_data["n_pieces"],vefa=param_sous_data["vefa"])
-
         return sous_data
     else:
         return transactions
@@ -66,23 +62,22 @@ def data_transformation(df):
     return df_scaled, prix_min, prix_max, n_pieces_min, n_pieces_max, surface_habitable_min, surface_habitable_max
 
 
-def generer_sous_dataset(df,surface,n_pieces,vefa,col,lim_nb_ligne=25,percent_ecart=0.25):
+def generer_sous_dataset(df,surface,n_pieces,vefa,col,lim_nb_ligne=25,percent_ecart=0.25,ecart_type):
+    #lim_nb_ligne est le nombre de ligne maximal du sous dataset final qui sera utiliser pour faire une prediction
+    
+    #percent_ecart est le pourcentage d'écart en surface que l'on va utiliser pour filtrer les transactions d'intéret - ex un appartement de 30m² +ou- 25%     
     ecart_surface = surface*percent_ecart
-    #df["date_transaction"] = df.date_transaction.apply(lambda x:pd.to_datetime(x))
+    
+
     df['sort_surf'] = df.surface_habitable.apply(lambda x: None if surface is None else abs(x - surface))
     df['sort_nb_p'] = df.n_pieces.apply(lambda x: None if n_pieces is None else abs(x - n_pieces))
     df['sort_vefa'] = df.vefa.apply(lambda x: None if vefa is None else x & vefa)
-    df = df[df['sort_surf'] <= ecart_surface]
+    df=df[(np.abs(stats.zscore(df['prix'])) < 4) & (df['sort_surf'] <= ecart_surface)]
+    
     df['prix_m2'] = df.apply(lambda x: x['prix'] / x['surface_habitable'], axis=1)
     prix_moy_m=df['prix_m2'].median()
     df['sort_prix']=df.prix_m2.apply(lambda x: abs(x-prix_moy_m))
-
-
-
-    #df['semester']= df.date_transaction.dt.year.astype(str) + 'S'+ np.where(df.date_transaction.dt.quarter.gt(2),2,1).astype(str)
-    #df["date_transaction"] =df.date_transaction.apply(lambda x:str(x.date()))
     df=df.sort_values(by=['semester','sort_vefa','sort_prix','sort_nb_p'], ascending=(False,False,True,True)).head(lim_nb_ligne)
-    print(df.head())
     df = df[col]
 
 
