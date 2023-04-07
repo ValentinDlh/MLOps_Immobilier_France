ec6d003f18e43e1ecb8c896adb4e70584681ffad
vlagou <112933180+vlagou@users.noreply.github.com>
2023-04-07 16:59:44 +0200

    Update prediction.py

--- BD/prediction.py
+++ BD/prediction.py
@@ -10,12 +10,15 @@ def prediction(departement, ville, quartier,vefa, n_pieces, surface_habitable,co
     Renvoie la prédiction de notre modèle basé sur les inputs de l'utilisateur.
     """
     # Détermination du dataset transactions transformé et récupération des valeurs max et min des variables
+    
+    #dictionnaire p contient les critères de filtre qui seront transmis dans une requête pour interroger la BD
     p={}
     p["vefa"]=vefa
     p["n_pieces"]=n_pieces
-
     p["surface_habitable"]=surface_habitable
-
+    
+    
+    #séléction des données pertinentes en fonction des critères du dictionnaire p
     df = data_selection(departement, ville,quartier,col,sous_dataset=True,param_sous_data= p)
 
 
@@ -24,6 +27,8 @@ def prediction(departement, ville, quartier,vefa, n_pieces, surface_habitable,co
         df)
 
     # Transformation des inputs de l'utilisateur
+    
+    #si l'utilisateur ne renseigne pas de nombre de pièces on prendra le nombre de pièce moyen du sous dataset
     if (n_pieces is None):
         n_pieces_norm= df.n_pieces.mean()
     else:
