37d98e1791170b16ab92421795889c453178e8f6
vlagou <112933180+vlagou@users.noreply.github.com>
2023-05-25 08:42:17 +0200

    Add files via upload
    Ajout de la fonction générer tdb_Quartier

--- BD/db.py
+++ BD/db.py
@@ -7,100 +7,115 @@ import numpy as np
 from pymongo.mongo_client import MongoClient
 from pymongo.server_api import ServerApi
 
-#Connection à la BD
-uri="mongodb+srv://vlago:DE_Immo_2023@cluster0.fv699mr.mongodb.net/?retryWrites=true&w=majority"
+# Connection à la BD
+#"mongodb+srv://vlago:DE_Immo_2023@cluster0.fv699mr.mongodb.net/?retryWrites=true&w=majority"
+
+uri = "mongodb://localhost:27017"
 client = MongoClient(uri, server_api=ServerApi('1'))
-db=client['Immo']
-collection=db['Transactions']
+db = client['Immo']
+collection = db['Transactions']
 
 
 # Obtenir une transaction par son ID
-def get_one(c:str):
-    return collection.find_one({"_id": c})
+def get_one(c: str):
+    return collection.find_one({"id_transaction": c})
+
 
 # extraire les données d'une requête dans la BD dans un dataframe
 
-def extract_from_DB_to_df_with_condition(condition:dict,col):
-
+def extract_from_DB_to_df_with_condition(condition: dict, col):
     df = pd.DataFrame.from_dict(json.loads(dumps(collection.find(condition))))
     df.set_index(['id_transaction'])
-    #transformation de la date en format datetime
-    df["date_transaction"] = df.date_transaction.apply(lambda x: pd.to_datetime(x['$date'],errors='coerce'))
-    
-    #affectation du semestre de la transaction
-    df['semester'] = df.date_transaction.dt.year.astype(str) + 'S' + np.where(df.date_transaction.dt.quarter.gt(2),2,1).astype(str)
-    
-    #selection des colonnes d'intéret dans le dataframe
-    df=df[col]
+    # transformation de la date en format datetime
+    df["date_transaction"] = df.date_transaction.apply(lambda x: pd.to_datetime(x['$date'], errors='coerce'))
+
+    # affectation du semestre de la transaction
+    df['semester'] = df.date_transaction.dt.year.astype(str) + 'S' + np.where(df.date_transaction.dt.quarter.gt(2), 2,
+                                                                              1).astype(str)
+
+    # selection des colonnes d'intéret dans le dataframe
+    df = df[col]
 
     return df
 
 
-#extraction des valeurs unique de la BD sur la colonne v
+# extraction des valeurs unique de la BD sur la colonne v
 
-def extract_distinct_value(condition:dict,v:str):
-
+def extract_distinct_value(condition: dict, v: str):
     return collection.find(condition).distinct(v)
 
 
-#extraction de toute la BD (sans requête) dans un dataframe - /!\trop long/!\  
+# extraction de toute la BD (sans requête) dans un dataframe - /!\trop long/!\
 def extract_from_DB_to_df(col):
-
     df = pd.DataFrame.from_dict(json.loads(dumps(collection.find())))
     df.set_index(['id_transaction'])
     df["date_transaction"] = df.date_transaction.apply(lambda x: pd.to_datetime(x['$date']).date())
     df['semester'] = df.date_transaction.dt.year.astype(str) + 'S' + np.where(df.date_transaction.dt.quarter.gt(2), 2,
                                                                               1).astype(str)
 
-    df=df[col]
+    df = df[col]
     return df
 
 
+def generer_tdb_quartier():
 
+    pipeline = [
+        {
+            '$match': {
+                'surface_habitable': {'$gt': 0},
+                'prix_m2': {'$gt': 500}
+            }
+        },
+        {
+            '$group': {
+                '_id': {
+                    'departement': '$departement',
+                    'NOM_COM': '$NOM_COM',
+                    #'NOM_IRIS':'$NOM_IRIS'
+                    #'ANNEE': {'$year': '$date_transaction'}
+                },
+                'Prix_moyen_m2': {'$avg': '$prix_m2'},
+                'Prix_moyen_m2_ecart_type': {'$stdDevPop': '$prix_m2'},
+                'AVG_surface_habitable': {'$avg': '$surface_habitable'},
+                'COUNT_id_transaction': {'$sum': 1}
+            }
+        },
+        {
+            "$project": {
+                "_id": 0,
+                "departement": "$_id.departement",
+                "NOM_COM": "$_id.NOM_COM",
+                #"ANNEE": "$_id.ANNEE",
+                "Prix_moyen_m2": {'$toInt': '$Prix_moyen_m2'},  # Conversion en entier
+                "Prix_moyen_m2_ecart_type": {'$toInt': '$Prix_moyen_m2_ecart_type'},
+                "Moyenne_surface_habitable": {'$toInt': '$AVG_surface_habitable'},
+                'COUNT_id_transaction': 1
+            }
+        },
+        {
+            '$sort': {
+                '_id.ANNEE': -1,
+                '_id.departement': 1
+            }
+        }
 
-#A faire
-def generate_tdb_quartier():
-    return None
+    ]
 
-    #return collection.find(condition)
+    result = db.Transactions.aggregate(pipeline)
+    collection_list=db.list_collection_names()
 
-#ajouter une colonne dans la BD prix/moyen par m²
-#Créer nouvelle table
-#_id
-#quartier
-#year
-#prix moyen/m²
-#prix median/m²
-#ecart_type m²
-#Volume transac
-#
+    for c in collection_list:
 
-'''class Transaction(BaseModel):
+        if c == 'Tdb_Quartier' :
+            db['Tdb_Quartier'].drop()
 
-    #ID : StringField(default=str(ObjectIdField()))
-    #adresse:str
-    #code_postal:int
-    #date_transaction:date
-    #DCOMIRIS:str
-    #departement:str
-    #prix:Optional[float]=None
-    #surface_habitable:int
-    #DEPCOM:str
-    #geometry:Point
-    #id_transaction:str
-    #id_ville:int
-    #IRIS:str
-    #latitude:str
-    #longitude:str
-    #n_pieces:Optional[int]=None
-    #NOM_COM:str
-    #NOM_IRIS:Optional[str]=None
-    #TYPE_IRIS:Optional[str]=None
-    #type_batiment:Optional[str]=None
-    #vefa:Optional[bool]
-    #ville:str'''
+    new_collection = db['Tdb_Quartier']
+      # Nouvelle collection pour enregistrer les résultats
 
+    documents_to_insert = list(result)
 
+    if documents_to_insert:
+        new_collection.insert_many(documents_to_insert)
 
 
 
