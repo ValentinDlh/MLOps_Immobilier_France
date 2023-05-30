8315ada11f51895b784a2f70f6629141f2feafb1
ValentinDlh <116817907+ValentinDlh@users.noreply.github.com>
2023-05-30 17:10:30 +0200

    Add files via upload

---
+++ Testing & Monitoring/immo_dag.py
@@ -0,0 +1,200 @@
+from airflow import DAG
+from airflow.utils.dates import days_ago
+from airflow.operators.python import PythonOperator
+import pandas as pd
+import datetime
+
+import import_new_dataset
+import test_unitaire
+import os
+import db
+
+
+my_immo_dag = DAG(
+    dag_id='immo_dag',
+    description='My Airflow MLOps project DAG',
+    tags=['MLOps_project_airflow_datascientest'],
+    #schedule_interval='0 0 1 6,12 *',
+    #start_date=datetime(2023, 6, 1),
+    default_args={
+        'owner': 'airflow',
+        'start_date': days_ago(0),
+    }
+)
+
+
+#task 1_0: test connexion BD
+
+def test_connexion_bd():
+
+    a=db.get_data_from_db({'id_transaction':107332}, 'Transactions')[0]['NOM_COM']
+    print(a)
+    return a
+
+
+
+
+# Task 1_1
+def import_new_data_main():
+    import_new_dataset.import_new_dataset()
+    print("import zip done !")
+    import_new_dataset.file_extract()
+    print("extract npz done !")
+    df = import_new_dataset.npz_to_df()
+
+    most_recent_date_new = import_new_dataset.get_date_max_new_transactions(df)
+    print(most_recent_date_new)
+    most_recent_date_old = import_new_dataset.get_date_max_old_transactions(import_new_dataset.collection)
+    print(most_recent_date_old)
+
+    if most_recent_date_new > most_recent_date_old:
+        new_transactions = df[df["date_transaction"] > most_recent_date_old]
+        print(new_transactions.columns)
+        new_data = import_new_dataset.batch_iris(new_transactions)
+        print(new_data.columns)
+        new_data_final = import_new_dataset.generate_new_transactions(new_data)
+        print(new_data_final.columns)
+        import_new_dataset.import_new_transactions(new_data_final)
+        print("done !")
+
+#Task1_2 : générer Tdb_quartier
+
+
+
+
+# Task 2 : récupérer les metriques dans le fichier json
+def get_all_metrics(task_instance):
+    date_last_DB = test_unitaire.get_metrics(test_unitaire.filename)['date_last_DB']
+    nb_ligne_bd_Transactions = test_unitaire.get_metrics(test_unitaire.filename)['nb_ligne_bd_Transactions']
+    nb_ligne_bd_Tdb_Quartier = test_unitaire.get_metrics(test_unitaire.filename)['nb_ligne_bd_Tdb_Quartier']
+    df_rqt = pd.DataFrame(test_unitaire.get_metrics(test_unitaire.filename)['df_rqt_test_transaction'])
+
+    print(date_last_DB)
+
+    task_instance.xcom_push(
+        key="nb_ligne_bd_Transactions",
+        value=nb_ligne_bd_Transactions
+    )
+    task_instance.xcom_push(
+        key="nb_ligne_bd_Tdb_Quartier",
+        value=nb_ligne_bd_Tdb_Quartier
+    )
+    task_instance.xcom_push(
+        key="df_rqt",
+        value=df_rqt
+    )
+
+
+
+
+# Task 3
+def test_unitaires(task_instance):
+    nb_ligne_bd_Transactions = task_instance.xcom_pull(
+        key="nb_ligne_bd_Transactions",
+        task_ids=['task_2']
+    )
+
+    nb_ligne_bd_Tdb_Quartier = task_instance.xcom_pull(
+        key="nb_ligne_bd_Tdb_Quartier",
+        task_ids=['task_2']
+    )
+
+    df_rqt = task_instance.xcom_pull(
+        key="df_rqt",
+        task_ids=['task_2']
+    )
+
+    d = {}
+    d['date_last_DB'] = str(datetime.now())
+    test_nb_ligne_bd_Transactions, d['nb_ligne_bd_Transactions'] = test_unitaire.test_nb_ligne_BD(
+        nb_ligne_bd_Transactions, 'Transactions')
+    test_nb_ligne_bd_Tdb_Quartier, d['nb_ligne_bd_Tdb_Quartier'] = test_unitaire.test_nb_ligne_BD(
+        nb_ligne_bd_Tdb_Quartier, 'Tdb_Quartier')
+    test_df_rqt_test_transaction, d['df_rqt_test_transaction'] = test_unitaire.test_rqt(df_rqt, 'Transactions')
+
+    task_instance.xcom_push(
+        key="d",
+        value=d
+    )
+    task_instance.xcom_push(
+        key="test_nb_ligne_bd_Transactions",
+        value=test_nb_ligne_bd_Transactions
+    )
+    task_instance.xcom_push(
+        key="test_nb_ligne_bd_Tdb_Quartier",
+        value=test_nb_ligne_bd_Tdb_Quartier
+    )
+    task_instance.xcom_push(
+        key="test_df_rqt_test_transaction",
+        value=test_df_rqt_test_transaction
+    )
+
+
+# Task 4
+def write_metrics_all(task_instance):
+    d = task_instance.xcom_pull(
+        key="d",
+        task_ids=['task_3']
+    )
+
+    test_nb_ligne_bd_Transactions = task_instance.xcom_pull(
+        key="test_nb_ligne_bd_Transactions",
+        task_ids=['task_3']
+    )
+
+    test_nb_ligne_bd_Tdb_Quartier = task_instance.xcom_pull(
+        key="test_nb_ligne_bd_Tdb_Quartier",
+        task_ids=['task_3']
+    )
+
+    test_df_rqt_test_transaction = task_instance.xcom_pull(
+        key="test_df_rqt_test_transaction",
+        task_ids=['task_3']
+    )
+    # écrire les valeurs
+    if (test_nb_ligne_bd_Transactions & test_nb_ligne_bd_Tdb_Quartier & test_df_rqt_test_transaction):
+        test_unitaire.write_metric(d, test_unitaire.filename)
+
+
+task_1_0 = PythonOperator(
+    task_id='test_connection_BD',
+    python_callable=test_connexion_bd,
+    dag=my_immo_dag,
+)
+
+
+task_1_1 = PythonOperator(
+    task_id='import_new_data',
+    python_callable=import_new_dataset.import_new_data_main,
+    dag=my_immo_dag
+)
+
+task_1_2 = PythonOperator(
+    task_id='generer_tdb_quartier',
+    python_callable=db.generer_tdb_quartier,
+    dag=my_immo_dag
+)
+
+task_2 = PythonOperator(
+    task_id='get_metrics_all',
+    python_callable=get_all_metrics,
+    dag=my_immo_dag
+)
+
+task_3 = PythonOperator(
+    task_id='test_unitaires',
+    python_callable=test_unitaires,
+    dag=my_immo_dag
+)
+
+task_4 = PythonOperator(
+    task_id='write_metrics_all',
+    python_callable=write_metrics_all,
+    dag=my_immo_dag
+)
+
+task_1_0 >> task_1_1
+task_1_1 >> task_1_2
+task_1_2 >> task_2
+task_2 >> task_3
+task_3 >> task_4
---
+++ Testing & Monitoring/import_new_dataset.py
@@ -0,0 +1,129 @@
+import pandas as pd
+import numpy as np
+import datetime
+from db import collection
+
+import geopandas as gpd
+from shapely.geometry import Point
+
+import subprocess
+from zipfile import ZipFile
+
+# spécifiant le nom du fichier zip
+file = "transactions.npz.zip"
+
+# Command Kaggle pour import new bdd
+command = "kaggle datasets download benoitfavier/immobilier-france -f transactions.npz"
+
+
+def import_new_dataset():
+    # Exécutez la commande dans le terminal
+    subprocess.run(command, shell=True)
+
+
+def file_extract():
+    # ouvrir le fichier zip en mode lecture
+    with ZipFile(file, 'r') as zip:
+        zip.extractall()
+
+
+def npz_to_df():
+    '''
+    Conversion du fichier transactions.npz en dataframe
+    '''
+    arrays = dict(np.load("transactions.npz"))
+    data = {k: [s.decode("utf-8") for s in v.tobytes().split(b"\x00")] if v.dtype == np.uint8 else v for k, v in
+            arrays.items()}
+    transactions = pd.DataFrame.from_dict(data)
+    return transactions
+
+
+def get_date_max_new_transactions(transactions):
+    '''
+    Retourne la date la plus récente de la BDD récente
+    '''
+    most_recent_date = max(transactions["date_transaction"])
+
+    # Extraire la valeur du timestamp en secondes
+    timestamp_seconds = most_recent_date.timestamp()
+
+    # Conversion du timestamp en objet datetime
+    most_recent_date_new = datetime.datetime.fromtimestamp(timestamp_seconds)
+
+    return most_recent_date_new
+
+
+def get_date_max_old_transactions(c):
+    '''
+    Retourne la date la plus récente de la BDD ancienne
+    '''
+    # Utiliser l'agrégation pour obtenir la date maximale
+    pipeline = [
+        {"$group": {"_id": None, "maxDate": {"$max": "$date_transaction"}}}
+    ]
+
+    result = list(c.aggregate(pipeline))
+
+    # Extraire la date maximale du résultat
+    most_recent_date_old = result[0]["maxDate"]
+
+    return most_recent_date_old
+
+
+def batch_iris(df):
+    geoiris = gpd.read_file("iris-2013-01-01/iris-2013-01-01.shp")
+
+    crs = {'init': 'epsg:4326'}
+    geometry = [Point(xy) for xy in zip(df.longitude, df.latitude)]
+    geo_transaction = gpd.GeoDataFrame(df, crs=crs, geometry=geometry)
+
+    return gpd.sjoin(geo_transaction, geoiris)
+
+
+def generate_new_transactions(new_df):
+    '''
+    Preprocessing et génération du dataframe final à intégrer dans mongodb
+    '''
+
+    new_df.drop(["index_right"], axis=1)
+    new_df = new_df.drop(["index_right"], axis=1)
+    new_df.drop_duplicates(inplace=True)
+
+    new_df = new_df[new_df["surface_habitable"] > 0]
+    new_df["prix_m2"] = int(new_df["prix"] / new_df["surface_habitable"])
+    new_df = new_df[
+        ["id_transaction", "date_transaction", "prix", "prix_m2", "adresse", "NOM_COM", "code_postal", "departement",
+         "NOM_IRIS", "TYP_IRIS", "vefa", "n_pieces", "surface_habitable"]]
+
+    return new_df
+
+
+def import_new_transactions(new_df):
+    # DF to dict
+    data = new_df.to_dict(orient='records')
+
+    # insertion des nouvelles transactions dans mongodb
+    collection.insert_many(data)
+
+
+def import_new_data_main():
+    import_new_dataset()
+    print("import zip done !")
+    file_extract()
+    print("extract npz done !")
+    df = npz_to_df()
+
+    most_recent_date_new = get_date_max_new_transactions(df)
+    print(most_recent_date_new)
+    most_recent_date_old = get_date_max_old_transactions(collection)
+    print(most_recent_date_old)
+
+    if most_recent_date_new > most_recent_date_old:
+        new_transactions = df[df["date_transaction"] > most_recent_date_old]
+        print(new_transactions.columns)
+        new_data = batch_iris(new_transactions)
+        print(new_data.columns)
+        new_data_final = generate_new_transactions(new_data)
+        print(new_data_final.columns)
+        import_new_transactions(new_data_final)
+        print("done !")
